1b8748b40e96c1e77d9a6edae34841effd2a2c34
kishor <kishoreyadavky37@gmail.com>
2026-03-31 23:13:53 +0530

    fix db debug

--- database.js
+++ database.js
@@ -1,11 +1,20 @@
-const mysql=require("mysql2")
+const mysql = require("mysql2");
 
-const db=mysql.createPool({
-   host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+const db = mysql.createPool({
+  host: process.env.MYSQLHOST || "localhost",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "kishor",
+  database: process.env.MYSQLDATABASE || "project",
+  port: process.env.MYSQLPORT || 3306
 });
 
-module.exports=db;
+db.getConnection((err, conn) => {
+  if (err) {
+    console.log("DB ERROR:", err);
+  } else {
+    console.log("DB CONNECTED");
+    conn.release();
+  }
+});
+
+module.exports = db;
