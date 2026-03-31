99b3d08eec445b4445a465a7b0bb1757e630d923
kishor <kishoreyadavky37@gmail.com>
2026-03-31 23:04:37 +0530

    fix railway

--- server.js
+++ server.js
@@ -1227,6 +1227,8 @@ app.get("/index/audit1",(req,res)=>{
 
 
 //run server
-app.listen(3000, () => {
-  console.log("server running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
